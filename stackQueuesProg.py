9bf50b5402fa7b73da5e4e00a5b2245ac176594d
Carljhn <zoletacarljohn1@gmail.com>
2022-12-10 11:34:22 -1200

    Second commit: Installed the Rich library and added the imports that I'll be using.

--- stackQueuesProg.py
+++ stackQueuesProg.py
@@ -14,3 +14,9 @@ from itertools import zip_longest
 from queue import LifoQueue, PriorityQueue, Queue
 from random import choice, randint
 from time import sleep
+
+from rich.align import Align
+from rich.columns import Columns
+from rich.console import Group
+from rich.live import Live
+from rich.panel import Panel
