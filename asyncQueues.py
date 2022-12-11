0a3953abb987a0721fa631a5afaa076ff9365421
Carljhn <zoletacarljohn1@gmail.com>
2022-12-11 10:24:11 -1200

    Eighth Commit: Added function parse_args.

--- asyncQueues.py
+++ asyncQueues.py
@@ -74,14 +74,24 @@ async def worker(worker_id, session, queue, links, max_depth):
         finally:
             queue.task_done()
 
+#Async def fetch_html
 async def fetch_html(session, url):
     async with session.get(url) as response:
         if response.ok and response.content_type == "text/html":
             return await response.text()
 
+#function parse_links
 def parse_links(url, html):
     soup = BeautifulSoup(html, features = "html.parser")
     for anchor in soup.select("a[href]"):
         href = anchor.get("href").lower()
         if not href.startswith("javascript:"):
             yield urljoin(url, href)
+
+#function parse_args
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url")
+    parser.add_argument("-d", "--max-depth", type = int, default = 2)
+    parser.add_argument("-w", "--num-workers", type = int, default = 3)
+    return parser.parse_args()
