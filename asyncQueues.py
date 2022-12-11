2a47e8b6f67d15caf38ef50e86a4589fbc99b7bd
Carljhn <zoletacarljohn1@gmail.com>
2022-12-11 10:11:06 -1200

    Fifth commit: Added async def worker

--- asyncQueues.py
+++ asyncQueues.py
@@ -56,3 +56,20 @@ async def main(args):
         display(links)
     finally:
         await session.close()
+
+#Async def worker
+async def worker(worker_id, session, queue, links, max_depth):
+    print(f"[{worker_id} starting]", file = sys.stderr)
+    while True:
+        url, depth = await queue.get()
+        links[url] += 1
+        try:
+            if depth <= max_depth:
+                print(f"[{worker_id} {depth=} {url=}]", file = sys.stderr)
+                if html := await fetch_html(session, url):
+                    for link_url in parse_links(url, html):
+                        await queue.put(Job(link_url, depth + 1))
+        except aiohttp.ClientError:
+            print(f"[{worker_id} failed at {url=}]", file = sys.stderr)
+        finally:
+            queue.task_done()
