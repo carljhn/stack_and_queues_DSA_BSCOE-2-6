4ffd7f94a879cd771a8ef071a09fd98fdfb94dc8
Carljhn <zoletacarljohn1@gmail.com>
2022-12-11 11:26:54 -1200

    Eighth Commit: Added a function named main.

--- multiprocessQueue.py
+++ multiprocessQueue.py
@@ -68,4 +68,37 @@ class Worker(multiprocessing.Process):
             if plaintext := job(self.hash_value):
                 self.queue_out.put(plaintext)
                 break
+
+#function main
+def main(args):
+    t1 = time.perf_counter
+    queue_in = multiprocessing.Queue()
+    queue_out = multiprocessing.Queue()
+
+    workers = [
+        Worker(queue_in, queue_out, args.hash_value)
+        for _ in range(args.num_workers)
+    ]
+
+    for worker in workers:
+        worker.start()
+
+    for text_length in range(1, args.max_length + 1):
+        combinations = Combinations(ascii_lowercase, text_length)
+        for indices in chunk_indices(len(combinations), len(workers)):
+            queue_in.put(Job(combinations, *indices))
+
+    queue_in.put(POISON_PILL)
+
+    while any(worker.is_alive() for worker in workers):
+        try:
+            solution = queue_out.get(timeout = 0.1)
+            if solution:
+                t2 = time.perf_counter()
+                print(f"{solution} (found in {t2 - t1:.1f}s)")
+                break
+        except queue.Empty:
+            pass
+    else:
+        print("Unable to find a solution")
         
