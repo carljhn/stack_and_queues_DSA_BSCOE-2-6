fc0c174172d3edf587eed832e852ac40c9e6ed5a
Carljhn <zoletacarljohn1@gmail.com>
2022-12-11 10:26:34 -1200

    Ninth Commit: Added a function display

--- asyncQueues.py
+++ asyncQueues.py
@@ -95,3 +95,8 @@ def parse_args():
     parser.add_argument("-d", "--max-depth", type = int, default = 2)
     parser.add_argument("-w", "--num-workers", type = int, default = 3)
     return parser.parse_args()
+
+#function display
+def display(links):
+    for url, count in links.most_common():
+        print(f"{count:>3} {url}")
