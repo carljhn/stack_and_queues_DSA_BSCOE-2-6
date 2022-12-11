d3e866a46208886152d47a43a6006761a0a90ce3
Carljhn <zoletacarljohn1@gmail.com>
2022-12-11 10:54:41 -1200

    Second Commit: Added a class named Combination and defined function __init__.

--- multiprocessQueue.py
+++ multiprocessQueue.py
@@ -8,3 +8,13 @@ import time
 from dataclasses import dataclass
 from hashlib import md5
 from string import ascii_lowercase
+
+#Variable POISON_PILL
+POISON_PILL = None
+
+#Class named Combinations
+class Combinations:
+    #function __init__
+    def __init__(self, alphabet, length):
+        self.alphabet = alphabet
+        self.length = length
