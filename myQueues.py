5d4ef6f0a47f44402348ef8a6ad06256ff44eb79
Carljhn <zoletacarljohn1@gmail.com>
2022-12-11 12:10:57 -1200

    Second Commit: Added a class named iterableMixin.

--- myQueues.py
+++ myQueues.py
@@ -8,3 +8,12 @@ from dataclasses import dataclass
 from heapq import heapify, heappop, heappush
 from itertools import count
 from typing import Any
+
+#Class IterableMixin
+class IterableMixin:
+    def __len__(self):
+        return len(self._elements)
+
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
